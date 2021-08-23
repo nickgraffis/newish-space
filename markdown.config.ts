c718c1668820e586edf3d2777ebac81cb469c246
Nick Graffis <nicholasgraffis@gmail.com>
2021-08-23 13:35:28 -0700

    trying async shiki

--- markdown.config.ts
+++ markdown.config.ts
@@ -6,42 +6,56 @@ import LinkAttributes from 'markdown-it-link-attributes'
 import Markdown from 'vite-plugin-md'
 import Footnote from 'markdown-it-footnote'
 import CodeCopy from './src/markdown-it-code-copy'
+// import 'prismjs/components/prism-regex'
+// import 'prismjs/components/prism-javascript'
+// import 'prismjs/components/prism-typescript'
+// import 'prismjs/components/prism-xml-doc'
+// import 'prismjs/components/prism-yaml'
+// import 'prismjs/components/prism-json'
+// import 'prismjs/components/prism-markdown'
+// import 'prismjs/components/prism-java'
+// import 'prismjs/components/prism-javadoclike'
+// import 'prismjs/components/prism-javadoc'
+// import 'prismjs/components/prism-jsdoc'
 export const markdownWrapperClasses = 'prose prose-sm m-auto text-left'
 
-export default Markdown({
-  wrapperComponent: 'post',
-  wrapperClasses: markdownWrapperClasses,
-  headEnabled: true,
-  markdownItOptions: {
-    quotes: '""\'\'',
-  },
-  markdownItSetup(md) {
-    md.use(CodeCopy, {
-      iconClass: 'heroicon heroicon-duplicate',
-      iconStyle: '',
-    })
-    md.use(LinkAttributes, {
-      pattern: /^https?:\/\//,
-      attrs: {
-        target: '_blank',
-        rel: 'noopener',
-      },
-    })
-    md.use(Emoji)
-    md.renderer.rules.text = (token, idx) => {
-      return twemoji.parse(token[idx].content, { className: 'twemoji' })
-    }
-    md.renderer.rules.emoji = (token, idx) => {
-      return twemoji.parse(token[idx].content, { className: 'twemoji' })
-    }
-    md.use(Anchor, {
-      permalink: true,
-      permalinkBefore: true,
-      permalinkSymbol: '#',
-    })
-    md.use(Footnote)
-    md.use(Shiki, {
-      theme: 'dracula',
-    })
-  },
-})
+export default async() => {
+  return Markdown({
+    wrapperComponent: 'post',
+    wrapperClasses: markdownWrapperClasses,
+    headEnabled: true,
+    markdownItOptions: {
+      quotes: '""\'\'',
+    },
+    markdownItSetup(md) {
+      md.use(CodeCopy, {
+        iconClass: 'heroicon heroicon-duplicate',
+        iconStyle: '',
+      })
+      md.use(LinkAttributes, {
+        pattern: /^https?:\/\//,
+        attrs: {
+          target: '_blank',
+          rel: 'noopener',
+        },
+      })
+      md.use(Emoji)
+      md.renderer.rules.text = (token, idx) => {
+        return twemoji.parse(token[idx].content, { className: 'twemoji' })
+      }
+      md.renderer.rules.emoji = (token, idx) => {
+        return twemoji.parse(token[idx].content, { className: 'twemoji' })
+      }
+      md.use(Anchor, {
+        permalink: true,
+        permalinkBefore: true,
+        permalinkSymbol: '#',
+      })
+      md.use(Footnote)
+      md.use(Shiki, {
+        theme: 'dracula',
+      })
+    // md.use(Prism)
+    },
+  })
+}
--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ import ViteIcons, { ViteIconsResolver } from 'vite-plugin-icons'
 import matter from 'gray-matter'
 // import { generate, test } from './previewify'
 // import { remove } from 'diacritics'
-import Markdown, { markdownWrapperClasses } from './markdown.config'
+import MarkdownSetup, { markdownWrapperClasses } from './markdown.config'
 
 // eslint-disable-next-line no-control-regex
 // const rControl = /[\u0000-\u001F]/g
@@ -37,73 +37,77 @@ import Markdown, { markdownWrapperClasses } from './markdown.config'
 
 // https://vitejs.dev/config/
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      '~/': `${path.resolve(__dirname, 'src')}/`,
-    },
-  },
-  plugins: [
-    Vue({
-      include: [/\.vue$/, /\.md$/],
-    }),
-    // https://github.com/antfu/vite-plugin-windicss
-    WindiCSS({
-      safelist: markdownWrapperClasses,
-    }),
-    // https://github.com/hannoeru/vite-plugin-pages
-    Pages({
-      pagesDir: 'src/pages',
-      extensions: ['vue', 'md'],
+export default defineConfig(async() => {
+  const MarkdownIt = await MarkdownSetup()
 
-      extendRoute(route) {
-        const p = path.resolve(__dirname, route.component.slice(1))
-        if (p.split('.').pop() === 'md') {
-          const md = fs.readFileSync(p, 'utf-8')
-          const { data, content } = matter(md) || { data: {}, content: '' }
-          const min = (md.split('').length / 6.7) / 200
-          data.duration = `${Math.ceil(min)} min read`
-          const removeScripts = content.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '')
-          const removeHTML = removeScripts.replace(/(<([^>]+)>)/ig, '')
-          data.content = markdownToTxt(removeHTML)
-          // if (data.title && data.description) generate(data.title, data.description, slugify(data.title)).then(() => console.log('done'))
-          route.meta = Object.assign(route.meta || {}, { frontmatter: data })
-        }
-        return route
+  return {
+    resolve: {
+      alias: {
+        '~/': `${path.resolve(__dirname, 'src')}/`,
       },
-    }),
-    // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
-    Layouts(),
-    // https://github.com/antfu/vite-plugin-md
-    Markdown,
-    ViteComponents({
-      globalComponentsDeclaration: true,
-      extensions: ['vue', 'md'],
-      customLoaderMatcher: path => path.endsWith('.md'),
-      customComponentResolvers: [
+    },
+    plugins: [
+      Vue({
+        include: [/\.vue$/, /\.md$/],
+      }),
+      // https://github.com/antfu/vite-plugin-windicss
+      WindiCSS({
+        safelist: markdownWrapperClasses,
+      }),
+      // https://github.com/hannoeru/vite-plugin-pages
+      Pages({
+        pagesDir: 'src/pages',
+        extensions: ['vue', 'md'],
+
+        extendRoute(route) {
+          const p = path.resolve(__dirname, route.component.slice(1))
+          if (p.split('.').pop() === 'md') {
+            const md = fs.readFileSync(p, 'utf-8')
+            const { data, content } = matter(md) || { data: {}, content: '' }
+            const min = (md.split('').length / 6.7) / 200
+            data.duration = `${Math.ceil(min)} min read`
+            const removeScripts = content.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '')
+            const removeHTML = removeScripts.replace(/(<([^>]+)>)/ig, '')
+            data.content = markdownToTxt(removeHTML)
+            // if (data.title && data.description) generate(data.title, data.description, slugify(data.title)).then(() => console.log('done'))
+            route.meta = Object.assign(route.meta || {}, { frontmatter: data })
+          }
+          return route
+        },
+      }),
+      // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
+      Layouts(),
+      // https://github.com/antfu/vite-plugin-md
+      MarkdownIt,
+      ViteComponents({
+        globalComponentsDeclaration: true,
+        extensions: ['vue', 'md'],
+        customLoaderMatcher: path => path.endsWith('.md'),
+        customComponentResolvers: [
         // https://github.com/antfu/vite-plugin-icons
-        ViteIconsResolver({
-          componentPrefix: '',
+          ViteIconsResolver({
+            componentPrefix: '',
           // enabledCollections: ['carbon']
-        }),
-      ],
-    }),
-    ViteIcons(),
-    // https://github.com/nickgraffis/vite-plugin-blurhash
-    Blurhash({
-      define: false,
-    }),
-  ],
-  server: {
-    fs: {
-      strict: true,
-    },
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8888/.netlify/functions',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ''),
+          }),
+        ],
+      }),
+      ViteIcons(),
+      // https://github.com/nickgraffis/vite-plugin-blurhash
+      Blurhash({
+        define: false,
+      }),
+    ],
+    server: {
+      fs: {
+        strict: true,
+      },
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8888/.netlify/functions',
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
+  }
 })
