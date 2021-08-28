2151636bf2e045a83044b75fb6b108d0c73eefcf
Nick Graffis <nicholasgraffis@gmail.com>
2021-08-27 22:53:52 -0700

    drop to node 12

--- src/logics/store.ts
+++ src/logics/store.ts
@@ -17,27 +17,27 @@ const gitHubData = ref({})
 
 const methods = {
   fetchGitHubData: async(repo: string) => {
-    let repoData: RepoData = {}
-    await get(`https://api.github.com/repos/${repo}`)
-      .then(res => repoData = {
-        description: res.data.description,
-        stars: res.data.stargazers_count,
-        forks: res.data.forks_count,
-        created_at: res.data.created_at,
-        updated_at: res.data.updated_at,
-        pushed_at: res.data.pushed_at,
-        homepage: res.data.homepage,
-        html_url: res.data.html_url,
-      })
-      .catch(err => console.error(err))
-    await get(`https://api.github.com/repos/${repo}/languages`)
-      .then(res => repoData.languages = res.data)
-      .catch(err => console.error(err))
+    const repoData: RepoData = {}
+    // await get(`https://api.github.com/repos/${repo}`)
+    //   .then(res => repoData = {
+    //     description: res.data.description,
+    //     stars: res.data.stargazers_count,
+    //     forks: res.data.forks_count,
+    //     created_at: res.data.created_at,
+    //     updated_at: res.data.updated_at,
+    //     pushed_at: res.data.pushed_at,
+    //     homepage: res.data.homepage,
+    //     html_url: res.data.html_url,
+    //   })
+    //   .catch(err => console.error(err))
+    // await get(`https://api.github.com/repos/${repo}/languages`)
+    //   .then(res => repoData.languages = res.data)
+    //   .catch(err => console.error(err))
 
-    gitHubData.value = {
-      ...gitHubData.value,
-      [repo]: repoData,
-    }
+    // gitHubData.value = {
+    //   ...gitHubData.value,
+    //   [repo]: repoData,
+    // }
   },
 }
 
